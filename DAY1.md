78153b0716938efd39ee2097c8e824200f295382
Andrew Yoo <andrewyoo@Andrews-MacBook-Pro.local>
2017-10-05 11:31:25 -0700

    first commit of the day :)

---
+++ DAY1.md
@@ -0,0 +1,12 @@
+
+# Goal for Day 1 : Oct 5
+
+- Get familiar with essentials of Vue.js and the scaffolding
+- Set up routing/components
+- Set up master page ( layout/style )
+
+# Nice to have
+
+- use LESS
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+
     {
       path: '/',
-      name: 'Hello',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'Vuewjs Scallfolding',
       component: Hello
+
     }
   ]
 })
