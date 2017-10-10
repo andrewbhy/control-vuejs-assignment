079f76203c5231f4945928554f736119b1d73e05
andrew <andrew@rationalsuite.com>
2017-10-09 17:39:28 -0700

    added vee-validator to login screen

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,26 +5,30 @@
             <div class="form-inner-content-container">
                 <h1>Login To View Or Add To-Dos</h1>
                 
-                
+     
                 <form v-on:submit.prevent="onSubmit">
                     <!--submit.prevent should prevent reloading-->
 
-                    <div class="login-input">
+                    <div class="login-container">
 
-                        <div class="input-group">
-                           
+                        <div class="username-input-group">
+                  
+                            <div>
                             <input name='username'
                                 :class="{'input': true, 'is-danger': errors.has('username') }" 
-                                v-validate="{ rules: { required: 'username',regex:  /^[a-zA-Z0-9._-]+$/} }" 
+                              
+                                v-validate:username="usernameValidationRule"
                                 v-model="username" @input="onInput"
                             >
                             </input>
+                            <button class="">Login</button>
+                            </div>
                             <i v-show="errors.has('username')" class="fa fa-warning"></i>
                             <span v-show="errors.has('username')" class="help is-danger">{{ errors.first('username') }}</span>
                         </div>
                         
                    
-                        <button class="">Login</button>
+                       
                     </div>
                   
 
@@ -46,10 +50,40 @@ import { createNamespacedHelpers } from 'vuex'
 const { mapActions } = createNamespacedHelpers('Auth');
 let actions = { ...mapActions(['login']) }
 
+import {Validator} from 'vee-validate';
+
 
 const usernameRegex = /^[a-zA-Z0-9._-]+$/;
 const usernameMinLength = 3;
 const usernameMaxLength = 15;
+const usernameValidationRule = `required|regex:^[a-zA-Z0-9._-]+$|max:${usernameMaxLength}|min:${usernameMinLength}`;
+
+let validationMessageDictionary = {
+
+    en : {
+        custom : {
+            username : {
+                required : 'Username is empty.',
+                regex :  'username must be alphanumeric (letters a-z,A-Z, numbers 0-9) and cannot contain spaces.',
+                max : `username cannot be longer than ${ usernameMaxLength } characters.`,
+                min : `username must be longer than ${ usernameMinLength } characters.`
+            }
+        }
+    }
+}
+
+
+let validator = new Validator({
+
+    username : `required|regex:/^[a-zA-Z0-9._-]+$/|max:{${usernameMaxLength}}|min:{${usernameMinLength}}`
+ 
+})
+
+//validator.validate('username', 'foo@bar.com');
+
+//  v-validate="{ rules: { required: 'username',regex:  /^[a-zA-Z0-9._-]+$/} }" 
+validator.updateDictionary(validationMessageDictionary);
+
 
 
 
@@ -65,6 +99,16 @@ export default {
         }
     },
 
+    created : function(){
+
+
+
+    },
+    computed : {
+        usernameValidationRule(){
+            return usernameValidationRule;
+        }
+    },
 
     methods: {
 
@@ -175,8 +219,13 @@ export default {
     text-align: center;
 }
 
-.login-input {
+.login-container {
     margin-top:25%;
+}
+.username-input-group {
+
+
+
 }
 
 #form-login {
