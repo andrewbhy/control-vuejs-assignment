c786cdd86829e495eb8cbf03de295c0265c4e0a2
Andrew Yoo <andrewyoo@Andrews-MacBook-Pro.local>
2017-10-10 17:06:55 -0700

    1.0.1

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -20,7 +20,7 @@ module.exports = merge(baseWebpackConfig, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': config.dev.env,
-      'process.env.VERSION': "'dev'"
+      'process.env.VERSION':  JSON.stringify('dev-'+require("../package.json").version)
     }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.HotModuleReplacementPlugin(),
